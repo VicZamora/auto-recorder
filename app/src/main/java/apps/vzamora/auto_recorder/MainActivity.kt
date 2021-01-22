4c5a5f8b6f52d82abb55f2ed9f758a977879d207
Víctor <vzamora11@gmail.com>
2021-01-22 16:47:41 +0100

    Creation of the Android Project
    Creation of the Android Project with an empty activity as the main activity.

---
+++ app/src/main/java/apps/vzamora/auto_recorder/MainActivity.kt
@@ -0,0 +1,11 @@
+package apps.vzamora.auto_recorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
